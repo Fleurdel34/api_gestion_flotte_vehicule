c01c4a31cce55d4ac284615713aab4b76785b1f0
Fleurdel34 <betdevelweb0953@gmail.com>
2025-01-13 19:14:20 +0100

    add VehiculeService and method

--- src/main/java/fr/studi/transport/ws/VehiculeController.java
+++ src/main/java/fr/studi/transport/ws/VehiculeController.java
@@ -1,27 +1,43 @@
 package fr.studi.transport.ws;
 
+import fr.studi.transport.pojo.Vehicule;
+import fr.studi.transport.service.VehiculeService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 
 
 @RestController
 @RequestMapping(value= ApiRegistration.API + ApiRegistration.REST_VEHICULE)
 public class VehiculeController {
 
+    private final VehiculeService vehiculeService;
+
+    public VehiculeController(VehiculeService vehiculeService) {
+        this.vehiculeService = vehiculeService;
+    }
+
     /**
-     *
-     * @return
+     * return all cars
+     * @return list of cars
      */
 
     @GetMapping
-    public String getAllVehicules(){
-        return "Hello World";
+    public List<Vehicule> getAllVehicules(){
+        return vehiculeService.getAllVehicules();
     }
 
-    @GetMapping("/{id}")
-    public String getVehiculeById(){
 
+    /**
+     * return only car
+     * @return one car
+     */
+
+    @GetMapping("/{id}")
+    public Vehicule getVehiculeById(@PathVariable Long id){
+        return vehiculeService.getVehiculeId(id);
     }
 }
