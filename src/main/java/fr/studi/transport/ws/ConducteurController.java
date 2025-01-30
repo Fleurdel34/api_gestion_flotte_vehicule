770ff8fa7b28d12d66fd7505c0aa789ec5cbeaf4
Fleurdel34 <betdevelweb0953@gmail.com>
2025-01-30 20:00:15 +0100

    create methods controller conducteur

--- src/main/java/fr/studi/transport/ws/ConducteurController.java
+++ src/main/java/fr/studi/transport/ws/ConducteurController.java
@@ -1,10 +1,37 @@
 package fr.studi.transport.ws;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import fr.studi.transport.pojo.Conducteur;
+import fr.studi.transport.service.ConducteurService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping()
+@RequestMapping(ApiRegistration.API + ApiRegistration.REST_CONDUCTEUR)
 public class ConducteurController {
+
+    @Autowired
+    private ConducteurService conducteurService;
+
+    @GetMapping
+    public List<Conducteur> getConducteur(){
+        return conducteurService.getAllConducteurs();
+    }
+
+    @GetMapping("/{id}")
+    public Conducteur getConducteurById(@PathVariable Long id){
+        return conducteurService.getConducteurId(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteConducteurById(@PathVariable Long id){
+        conducteurService.deleteConducteurById(id);
+    }
+
+    @PostMapping
+    public Conducteur createConducteur(@RequestBody Conducteur conducteur){
+        return conducteurService.createConducteur(conducteur);
+    }
 
 }
