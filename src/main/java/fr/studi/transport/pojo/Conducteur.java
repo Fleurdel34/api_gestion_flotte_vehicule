12cc9996d722d759c6ec6c9af41a6b4cb723dce8
Fleurdel34 <betdevelweb0953@gmail.com>
2025-02-13 21:40:30 +0100

    configuration database

--- src/main/java/fr/studi/transport/pojo/Conducteur.java
+++ src/main/java/fr/studi/transport/pojo/Conducteur.java
@@ -1,12 +1,64 @@
 package fr.studi.transport.pojo;
 
-import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
+@Entity
 public class Conducteur {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long conducteurId;
     private String nom;
     private String prenom;
     private Long vehiculeId;
+
+    public Conducteur() {
+    }
+
+    public Conducteur(Long conducteurId, String nom, String prenom, Long vehiculeId) {
+        this.conducteurId = conducteurId;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.vehiculeId = vehiculeId;
+    }
+
+
+    public Long getConducteurId() {
+        return conducteurId;
+    }
+
+    public void setConducteurId(Long conducteurId) {
+        this.conducteurId = conducteurId;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public Long getVehiculeId() {
+        return vehiculeId;
+    }
+
+    public void setVehiculeId(Long vehiculeId) {
+        this.vehiculeId = vehiculeId;
+    }
 }
