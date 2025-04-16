9d3d1eef7eac21c83cb88a1ebead03ab8093c802
Fleurdel34 <betdevelweb0953@gmail.com>
2025-04-16 17:32:48 +0200

    update security

---
+++ src/main/java/fr/studi/transport/security/jwt/JwtUtils.java
@@ -0,0 +1,57 @@
+package fr.studi.transport.security.jwt;
+
+
+import fr.studi.transport.security.service.UserDetailsImpl;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+
+
+
+@Component
+public class JwtUtils {
+
+    @Value("${transport.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${transport.jwtExprationMs}")
+    private int jwtExpirationInMs;
+
+    public String generateToken(Authentication authentication){
+
+        UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
+
+        return Jwts.builder()
+                .setSubject(userPrincipal.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() +jwtExpirationInMs))
+                .signWith(key(), SignatureAlgorithm.HS256)
+                .compact();
+
+    }
+
+    private Key key(){
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public String getUsernameFromJwtToken(String token){
+        return Jwts.parser()
+                .setSigningKey(key()).build()
+                .parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateJwtToken(String token){
+        try {
+            Jwts.parser()
+                    .setSigningKey(key()).build().parse(token);
+        }
+    }
+}
